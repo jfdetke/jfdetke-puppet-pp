d4a14197fa054c7efd37d7e73bdb6b784a39e9df
john detke <jdetke@jdetke-ThinkPad-T61.(none)>
2012-07-19 20:04:32 -0700

    moved to ldetke

--- users.pp
+++ users.pp
@@ -1,17 +1,13 @@
-  user { "jdetke":
+  user { "ldetke":
     ensure => "present",
     uid => "1007",
-    gid => "1000",
-    comment => "Shaken, not stirred",
-    home => "/home/jdetke",
+    gid => "1007",
+    home => "/home/7detke",
     # this next line creates the home dir
     managehome => true,            
- #  this next line changes their password in /etc/shadow! Grab the first part of the password only with:
- #  `cat /etc/shadow | grep $username| cut -f 2 -d : `  and add it here in single quotes:
-    password => '$1$syYkvOHY$ZxAabcdEFG1'
     }
 
- group { "jdetke":
+ group { "ldetke":
 	ensure => "present",
-	gid    => 1000,
+	gid    => 1007,
 }
